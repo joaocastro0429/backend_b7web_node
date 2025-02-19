85770be01dc6b028debae6a0a02856cc61e9e78a
joaocastro <joaocastro0429@gmail.com>
2025-02-19 20:12:39 -0300

    add scripts

---
+++ introducao_node/dist/teste.js
@@ -0,0 +1,4 @@
+"use strict";
+let nome = "joao";
+let idade = 36;
+console.log(`ola, meu nome é ${nome} e minha idade é ${idade}`);
--- introducao_node/teste.js
+++
@@ -1,4 +0,0 @@
-let nome="joao"
-let idade = 36
-
-console.log(`ola, meu nome é ${nome} e minha idade é ${idade}`)
